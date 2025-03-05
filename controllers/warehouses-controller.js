0246eb19e728898f6cedcb21fd30041f1cd7a821
Alexandria Balkaran <alexandriabalkaran@gmail.com>
2025-03-05 15:59:46 -0500

    added remove function

--- controllers/warehouses-controller.js
+++ controllers/warehouses-controller.js
@@ -13,4 +13,24 @@ const index = async (_req, res) => {
   }
 };
 
-export { index };
+const remove = async (req, res) => {
+  try {
+    const rowsDeleted = await knex("warehouses")
+      .where({ id: req.params.id })
+      .delete();
+
+    if (rowsDeleted === 0) {
+      return res
+        .status(404)
+        .json({ message: `Warehouse with ID ${req.params.id} not found` });
+    }
+    res.sendStatus(204);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: `Unable to delete warehouse: ${error}`,
+    });
+  }
+};
+
+export { index, remove };
