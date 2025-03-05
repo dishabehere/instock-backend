3c6040ed0aa15de0f37fc77907578a8fcc12bfb1
candiceojleung <candice.leung@live.ca>
2025-03-04 20:34:38 -0500

    import warehouse list route to index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+import warehousesRoutes from "./routes/warehousesRoutes.js"
+
 const app = express();
 
 // Middleware
@@ -9,9 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // Basic Route
-app.get('/', (req, res) => {
-    res.send('Welcome to the Express Server!');
-});
+app.get("/" , warehousesRoutes); 
 
 // Server Listening
 const PORT = process.env.PORT || 8080;
