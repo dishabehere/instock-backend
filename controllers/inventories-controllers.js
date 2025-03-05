5adfb73c30648a04399cd748b2f990170739579f
candiceojleung <candice.leung@live.ca>
2025-03-05 11:54:14 -0500

    create inventories path and join table with warehouse table to pull warehouse name along with list of all inventory items

---
+++ controllers/inventories-controllers.js
@@ -0,0 +1,25 @@
+import initKnex from "knex";
+import configuration from "../knexfile.js";
+const knex = initKnex(configuration);
+
+const index = async (_req, res) => {
+  try {
+    const rawData = await knex("inventories")
+      .select("inventories.*", "warehouses.warehouse_name")
+      .join("warehouses", "inventories.warehouse_id", "=", "warehouses.id");
+
+    const data = rawData.map(
+      ({ updated_at, created_at, warehouse_id, ...rest }) => ({
+        ...rest,
+        warehouse_name: rest.warehouse_name,
+      })
+    );
+    console.log("called", data);
+    res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(`Error retrieving Inventories List: ${error}`);
+  }
+};
+
+export { index };
